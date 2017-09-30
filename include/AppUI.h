153bcc0ae703342a34281a82e24bce21ac0ed2c8
rezaali <syed.reza.ali@gmail.com>
2017-09-29 21:09:08 -0700

    minor update

--- include/AppUI.h
+++ include/AppUI.h
@@ -30,7 +30,7 @@ SOFTWARE.
 #include "GlslParams.h"
 #include "UI.h"
 
-#ifdef USE_WINDOW_CANVAS
+#if USE_WINDOW_CANVAS
 typedef reza::ui::WindowCanvas UIPanel;
 typedef reza::ui::WindowCanvasRef UIPanelRef;
 #else
